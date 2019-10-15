c41af8c195eb2674dbfb2a466e26328b277989de
0x6c6868 <1023347025@qq.com>
2019-10-15 20:52:58 +0800

    ch03-08

---
+++ src/CodingInterviews/ch02/NextNodeInBinaryTrees.java
@@ -0,0 +1,68 @@
+package CodingInterviews.ch02;
+
+import CodingInterviews.util.CharTreeNode;
+
+public class NextNodeInBinaryTrees {
+
+    static CharTreeNode findNextNode(CharTreeNode node) {
+
+        if (node == null) return null;
+
+        if (node.right != null) {
+            CharTreeNode rstNode = node.right;
+
+            while (rstNode.left != null)
+                rstNode = rstNode.left;
+
+            return rstNode;
+        } else if (node.parent != null) {
+            CharTreeNode parent = node.parent;
+
+            while (parent != null && parent.right == node) {
+                node = node.parent;
+                parent = node.parent;
+            }
+
+            return parent;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        CharTreeNode a = new CharTreeNode('a');
+        CharTreeNode b = new CharTreeNode('b');
+        CharTreeNode c = new CharTreeNode('c');
+        CharTreeNode d = new CharTreeNode('d');
+        CharTreeNode e = new CharTreeNode('e');
+        CharTreeNode f = new CharTreeNode('f');
+        CharTreeNode g = new CharTreeNode('g');
+        CharTreeNode h = new CharTreeNode('h');
+        CharTreeNode i = new CharTreeNode('i');
+
+        a.left = b;
+        a.right = c;
+        b.parent = a;
+        c.parent = a;
+
+        b.left = d;
+        b.right = e;
+        d.parent = b;
+        e.parent = b;
+
+        e.left = h;
+        e.right = i;
+        h.parent = e;
+        i.parent = e;
+
+        c.left = f;
+        c.right = g;
+        f.parent = c;
+        f.parent = c;
+
+        CharTreeNode nextNode = findNextNode(a);
+        System.out.println(nextNode.val);
+
+    }
+}
---
+++ src/CodingInterviews/util/CharTreeNode.java
@@ -0,0 +1,12 @@
+package CodingInterviews.util;
+
+public class CharTreeNode {
+    public char val;
+    public CharTreeNode left;
+    public CharTreeNode right;
+    public CharTreeNode parent;
+
+    public CharTreeNode(char x) {
+        val = x;
+    }
+}
