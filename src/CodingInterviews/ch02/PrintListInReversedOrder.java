11f8e7fa40f0ee62840dfe95bbd8bc8ff0ccbccc
0x6c6868 <1023347025@qq.com>
2019-10-15 14:38:01 +0800

    ch03-06

---
+++ src/CodingInterviews/ch02/PrintListInReversedOrder.java
@@ -0,0 +1,45 @@
+package CodingInterviews.ch02;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class PrintListInReversedOrder {
+
+    static public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack = new Stack();
+
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+
+        ArrayList<Integer> rst = new ArrayList<>(stack.size());
+        while (!stack.isEmpty()) {
+            rst.add(stack.pop());
+        }
+
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(67);
+        ListNode n2 = new ListNode(0);
+        ListNode n3 = new ListNode(24);
+        ListNode n4 = new ListNode(58);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+
+        printListFromTailToHead(n1);
+    }
+}
