426eab60f3f9a895b26cf0617ad22f860fa16b1b
0x6c6868 <1023347025@qq.com>
2019-10-16 13:33:05 +0800

    ch03-10

---
+++ src/CodingInterviews/ch02/Fibonacci.java
@@ -0,0 +1,24 @@
+package CodingInterviews.ch02;
+
+public class Fibonacci {
+    public int fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        int fibOne = 0;
+        int fibTwo = 1;
+        int fibN = fibTwo;
+        for (int i = 2; i <= n; i++) {
+            fibN = fibOne + fibTwo;
+
+            fibOne = fibTwo;
+            fibTwo = fibN;
+        }
+        return fibN;
+
+    }
+
+    public static void main(String[] args) {
+    }
+}
