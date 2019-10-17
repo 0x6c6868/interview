57388a6042e18b514494d677b6bcfd9791be2e86
0x6c6868 <1023347025@qq.com>
2019-10-17 18:50:30 +0800

    ch03-14 ch03-15

---
+++ src/CodingInterviews/ch02/CuttingRope.java
@@ -0,0 +1,41 @@
+package CodingInterviews.ch02;
+
+public class CuttingRope {
+
+    static int maxProductAfterCutting(int length) {
+        if (length < 2) return 0;
+        if (length == 2) return 1;
+        if (length == 3) return 2;
+
+        int[] maxProduct = new int[length + 1];
+        maxProduct[0] = 0;
+        maxProduct[1] = 1;
+        maxProduct[2] = 2;
+        maxProduct[3] = 3;
+
+        for (int i = 4; i <= length; i++) {
+            int max = 0;
+            for (int j = 1; j <= i / 2; j++) {
+                int tmp = maxProduct[j] * maxProduct[i - j];
+                if (tmp > max) {
+                    max = tmp;
+                }
+            }
+            maxProduct[i] = max;
+        }
+        return maxProduct[length];
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(maxProductAfterCutting(1) == 0);
+        //System.out.println(maxProductAfterCutting(2) == 1);
+        //System.out.println(maxProductAfterCutting(3) == 2);
+        System.out.println(maxProductAfterCutting(4));
+        System.out.println(maxProductAfterCutting(5));
+        System.out.println(maxProductAfterCutting(6));
+        System.out.println(maxProductAfterCutting(7));
+        System.out.println(maxProductAfterCutting(8));
+        System.out.println(maxProductAfterCutting(9));
+        System.out.println(maxProductAfterCutting(10));
+    }
+}
---
+++ src/CodingInterviews/ch02/NumberOf1InBinary.java
@@ -0,0 +1,17 @@
+package CodingInterviews.ch02;
+
+public class NumberOf1InBinary {
+
+    static int NumberOf1(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(NumberOf1(-2147483648));
+    }
+}
