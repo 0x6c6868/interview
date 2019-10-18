d6d61168cbe84f14965c2be6c2b40e9e81fbcaa8
0x6c6868 <1023347025@qq.com>
2019-10-18 16:29:17 +0800

    ch03-16

---
+++ src/CodingInterviews/ch03/Power.java
@@ -0,0 +1,28 @@
+package CodingInterviews.ch03;
+
+public class Power {
+    public double Power(double base, int exponent) {
+        int realExponent = exponent;
+        if (exponent < 0) {
+            if (base == 0) {
+                throw new RuntimeException("输入不合法");
+            }
+            realExponent = -exponent;
+        }
+
+        double rst = 1;
+        while (realExponent > 0) {
+            if ((realExponent & 1) > 0) {
+                rst *= base;
+                realExponent -= 1;
+            } else {
+                rst *= rst;
+                realExponent /= 2;
+            }
+        }
+        return exponent > 0 ? rst : 1 / rst;
+    }
+
+    public static void main(String[] args) {
+    }
+}
