20e92787e794591c5ef43a89fac727127cfa721c
0x6c6868 <1023347025@qq.com>
2019-11-12 21:28:51 +0800

    ch05-39

--- src/CodingInterviews/_39_MoreThanHalfNumber.java
+++ src/CodingInterviews/_39_MoreThanHalfNumber.java
@@ -4,28 +4,48 @@ public class _39_MoreThanHalfNumber {
     public int MoreThanHalfNum_Solution(int[] array) {
         if (array == null || array.length == 0) return 0;
 
-        int result = array[0];
-        int times = 1;
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] == result) {
-                times++;
+        int start = 0;
+        int end = array.length - 1;
+        int index = Partition(array, start, end);
+        while (index != array.length / 2) {
+            if (index > array.length / 2) {
+                end = index - 1;
+                index = Partition(array, start, end);
             } else {
-                if (times == 0) {
-                    result = array[i];
-                    times = 1;
-                } else {
-                    times--;
+                start = index + 1;
+                index = Partition(array, start, end);
+            }
+        }
+
+        int times = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == array[index]) {
+                times++;
+            }
+        }
+
+        return times > array.length / 2 ? array[index] : 0;
+    }
+
+    public int Partition(int[] array, int start, int end) {
+        int small = start - 1;
+        for (int i = start; i < end; i++) {
+            if (array[i] < array[end]) {
+                small++;
+                if (small != i) {
+                    int tmp = array[i];
+                    array[i] = array[small];
+                    array[small] = tmp;
                 }
             }
         }
 
-        times = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == result) times++;
-            else times--;
-        }
+        small++;
+        int tmp = array[end];
+        array[end] = array[small];
+        array[small] = tmp;
 
-        return times > 0 ? result : 0;
+        return small;
     }
 
 }
