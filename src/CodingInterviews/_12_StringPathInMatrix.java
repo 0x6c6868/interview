76d6937ab8077ae05147663fafa9622340bdd5b3
0x6c6868 <1023347025@qq.com>
2019-11-13 22:32:49 +0800

    ch02-12

--- src/CodingInterviews/_12_StringPathInMatrix.java
+++ src/CodingInterviews/_12_StringPathInMatrix.java
@@ -2,7 +2,7 @@ package CodingInterviews;
 
 public class _12_StringPathInMatrix {
 
-    static boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
 
         if (matrix == null || rows <= 0 || cols <= 0) {
             return false;
@@ -20,11 +20,11 @@ public class _12_StringPathInMatrix {
         return false;
     }
 
-    static boolean hasPathCore(char[] matrix,
-                               int rows, int cols,
-                               int row, int col,
-                               boolean[][] visited,
-                               char[] str, int index) {
+    private boolean hasPathCore(char[] matrix,
+                                int rows, int cols,
+                                int row, int col,
+                                boolean[][] visited,
+                                char[] str, int index) {
         // 到底了
         if (index >= str.length) {
             return true;
@@ -53,36 +53,4 @@ public class _12_StringPathInMatrix {
         return false;
     }
 
-    public static void main(String[] args) {
-        char[] matrix1 = {
-                'A', 'B', 'T', 'G',
-                'C', 'F', 'C', 'S',
-                'J', 'D', 'E', 'H'
-        };
-        System.out.println(hasPath(matrix1, 3, 4, new char[]{'B', 'F', 'C', 'E'}));
-
-        char[] matrix2 = {
-                'A', 'B', 'C', 'E',
-                'S', 'F', 'C', 'S',
-                'A', 'D', 'E', 'E'
-        };
-        System.out.println(hasPath(matrix2, 3, 4, new char[]{'A', 'B', 'C', 'C', 'E', 'D'}));
-
-        char[] matrix3 = {
-                'A', 'B', 'T', 'G',
-                'C', 'F', 'C', 'S',
-                'J', 'D', 'E', 'H'
-        };
-        System.out.println(!hasPath(matrix3, 3, 4, new char[]{'A', 'B', 'F', 'B'}));
-
-        char[] matrix4 = {
-                'A', 'B', 'C', 'E', 'H', 'J', 'I', 'G',
-                'S', 'F', 'C', 'S', 'L', 'O', 'P', 'Q',
-                'A', 'D', 'E', 'E', 'M', 'N', 'O', 'E',
-                'A', 'D', 'I', 'D', 'E', 'J', 'F', 'M',
-                'V', 'C', 'E', 'I', 'F', 'G', 'G', 'S',
-        };
-        System.out.println(hasPath(matrix4, 5, 8, new char[]{'S', 'L', 'H', 'E', 'C', 'C', 'E', 'I', 'D', 'E', 'J', 'F', 'G', 'G', 'F', 'I', 'E'}));
-
-    }
 }
