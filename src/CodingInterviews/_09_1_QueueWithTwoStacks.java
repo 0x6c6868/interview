eb69706f38c5c53e9c925bb3a082203f91d2555c
0x6c6868 <1023347025@qq.com>
2019-11-13 22:20:02 +0800

    ch02-9g

--- src/CodingInterviews/_09_1_QueueWithTwoStacks.java
+++ src/CodingInterviews/_09_1_QueueWithTwoStacks.java
@@ -4,10 +4,10 @@ import java.util.Stack;
 
 public class _09_1_QueueWithTwoStacks {
 
-    static Stack<Integer> stack1 = new Stack<Integer>();
-    static Stack<Integer> stack2 = new Stack<Integer>();
+    private Stack<Integer> stack1 = new Stack<Integer>();
+    private Stack<Integer> stack2 = new Stack<Integer>();
 
-    static void push(int node) {
+    public void push(int node) {
         if (stack1.isEmpty()) {
             while (!stack2.isEmpty()) {
                 stack1.push(stack2.pop());
@@ -17,7 +17,7 @@ public class _09_1_QueueWithTwoStacks {
         stack1.push(node);
     }
 
-    static int pop() {
+    private int pop() {
         if (stack2.isEmpty()) {
             while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
@@ -27,20 +27,4 @@ public class _09_1_QueueWithTwoStacks {
         return stack2.pop();
     }
 
-    public static void main(String[] args) {
-        push(1);
-        push(2);
-        push(3);
-
-        pop();
-        pop();
-
-        push(4);
-        pop();
-
-        push(5);
-        pop();
-        pop();
-
-    }
 }
