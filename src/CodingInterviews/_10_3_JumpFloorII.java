4eadc80c45b3f7ebab632ba9b1234e5852793684
0x6c6868 <1023347025@qq.com>
2019-11-13 22:29:37 +0800

    ch02-10

---
+++ src/CodingInterviews/_10_3_JumpFloorII.java
@@ -0,0 +1,18 @@
+package CodingInterviews;
+
+public class _10_3_JumpFloorII {
+
+    public int JumpFloorII(int target) {
+        if (target <= 0) {
+            return 0;
+        }
+
+        int rst = 1;
+        for (int i = 1; i < target; i++) {
+            rst *= 2;
+        }
+
+        return rst;
+    }
+
+}
