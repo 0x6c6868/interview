c135950b1f18c3badaae42f329416bacb45d2055
0x6c6868 <1023347025@qq.com>
2019-11-20 22:08:10 +0800

    ch06-62

---
+++ src/CodingInterviews/_62_LastNumberInCircle.java
@@ -0,0 +1,14 @@
+package CodingInterviews;
+
+public class _62_LastNumberInCircle {
+
+    public int LastRemaining_Solution(int n, int m) {
+        if (n < 1 || m < 1) return -1;
+
+        int last = 0;
+        for (int i = 2; i <= n; i++) last = (last + m) % i;
+
+        return last;
+    }
+
+}
