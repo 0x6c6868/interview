d0b712ed06305e5febb3cfc9b799f9af7b05d782
0x6c6868 <1023347025@qq.com>
2019-11-20 22:45:25 +0800

    ch06-65

---
+++ src/CodingInterviews/_65_AddTwoNumbers.java
@@ -0,0 +1,18 @@
+package CodingInterviews;
+
+public class _65_AddTwoNumbers {
+
+    public int Add(int num1, int num2) {
+        int sum, carry;
+
+        do {
+            sum = num1 ^ num2;
+            carry = (num1 & num2) << 1;
+            num1 = sum;
+            num2 = carry;
+        } while (num2 != 0);
+
+        return num1;
+    }
+
+}
