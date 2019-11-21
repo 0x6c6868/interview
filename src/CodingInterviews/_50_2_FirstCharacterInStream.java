2fb55f1c932fa5882fd66f9358c2d7148e774dc7
0x6c6868 <1023347025@qq.com>
2019-11-21 17:59:50 +0800

    No.50

--- src/CodingInterviews/_50_2_FirstCharacterInStream.java
+++ src/CodingInterviews/_50_2_FirstCharacterInStream.java
@@ -1,5 +1,26 @@
 package CodingInterviews;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class _50_2_FirstCharacterInStream {
-    // TODO
+
+    private Map<Character, Integer> charCount = new HashMap<>();
+
+    private List<Character> data = new ArrayList<>();
+
+    public void Insert(char ch) {
+        charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+        data.add(ch);
+    }
+
+    public char FirstAppearingOnce() {
+        for (Character ch : data) {
+            if (charCount.get(ch) == 1) return ch;
+        }
+        return '#';
+    }
+
 }
