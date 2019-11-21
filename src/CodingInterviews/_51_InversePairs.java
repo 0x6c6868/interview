b8bb77d2ef325daa5a16b3c9162376d0afd0d73f
0x6c6868 <1023347025@qq.com>
2019-11-21 22:15:04 +0800

    CopyFromInter

--- src/CodingInterviews/_51_InversePairs.java
+++ src/CodingInterviews/_51_InversePairs.java
@@ -1,58 +1,40 @@
 package CodingInterviews;
 
-/**
- * TODO AC不过
- */
 public class _51_InversePairs {
 
-    public int InversePairs(int[] array) {
-        if (array == null || array.length == 0) return 0;
-        int[] copy = new int[array.length];
-        return InversePairsCore(array, copy, 0, array.length - 1);
+    private int cnt;
+
+    private void Sort(int[] array, int start, int end) {
+        if (start >= end) return;
+        int mid = (start + end) / 2;
+        Sort(array, start, mid);
+        Sort(array, mid + 1, end);
+        Merge(array, start, mid, end);
     }
 
-    private int InversePairsCore(int[] array, int[] copy, int start, int end) {
-        if (start == end) {
-            copy[start] = array[start];
-            return 0;
-        }
-
-        int mid = (end - start) / 2 + start;
-
-        int left = InversePairsCore(array, copy, start, mid);
-        int right = InversePairsCore(array, copy, mid + 1, end);
-
-        int count = 0;
-        int p1 = mid;
-        int p2 = end;
-        int indexCopy = end;
-        while (p1 >= start && p2 >= mid + 1) {
-            if (p1 >= start && array[p1] > array[p2]) {
-                copy[indexCopy] = array[p1];
-                p1--;
-                count += (p2 - mid) % 1000000007;
-            } else {
-                copy[indexCopy] = array[p2];
-                p2--;
+    private void Merge(int[] array, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int k = 0, i = start, j = mid + 1;
+        while (i <= mid && j <= end) {
+            if (array[i] <= array[j])
+                temp[k++] = array[i++];
+            else {
+                temp[k++] = array[j++];
+                cnt = (cnt + (mid - i + 1)) % 1000000007;
             }
-            indexCopy--;
         }
-
-        while (p1 >= start) {
-            copy[indexCopy] = array[p1];
-            p1--;
-            indexCopy--;
+        while (i <= mid)
+            temp[k++] = array[i++];
+        while (j <= end)
+            temp[k++] = array[j++];
+        for (int l = 0; l < k; l++) {
+            array[start + l] = temp[l];
         }
+    }
 
-        while (p2 >= mid + 1) {
-            copy[indexCopy] = array[p2];
-            p2--;
-            indexCopy--;
-        }
-
-        for (int i = start; i <= end; i++) array[i] = copy[i];
-
-        return (left + right + count) % 1000000007;
+    public int InversePairs(int[] array) {
+        Sort(array, 0, array.length - 1);
+        return cnt;
     }
 
 }
