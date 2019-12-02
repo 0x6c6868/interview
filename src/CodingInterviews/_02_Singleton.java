71318d8b47667319bd463cf8a1bd8c52462767ff
0x6c6868 <1023347025@qq.com>
2019-12-02 14:33:11 +0800

    real lock bro

--- src/CodingInterviews/_02_Singleton.java
+++ src/CodingInterviews/_02_Singleton.java
@@ -1,24 +1,32 @@
 package CodingInterviews;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class _02_Singleton {
 
-    private volatile static _02_Singleton instance;
+    private static _02_Singleton instance;
+
+    private static Lock lock = new ReentrantLock();
 
     private _02_Singleton() {
     }
 
     public static _02_Singleton getInstance() {
         if (instance == null) {
-            synchronized (_02_Singleton.class) {
+            try {
+                lock.lock();
                 if (instance == null) {
                     instance = new _02_Singleton();
                 }
+            } finally {
+                lock.unlock();
             }
         }
         return instance;
     }
 
     public static void main(String[] args) {
-        System.out.println(_02_Singleton.getInstance());
+        System.out.println(_02_Singleton.getInstance() == _02_Singleton.getInstance());
     }
 }
