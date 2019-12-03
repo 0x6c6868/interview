29b647ad27e4b05a5881d87e4c94adecd3971841
0x6c6868 <1023347025@qq.com>
2019-12-03 16:46:04 +0800

    fix three

--- src/CodingInterviews/_24_1_ReverseList.java
+++ src/CodingInterviews/_24_1_ReverseList.java
@@ -2,25 +2,21 @@ package CodingInterviews;
 
 public class _24_1_ReverseList {
 
-    public ListNode reverseList(ListNode head) {
+    public ListNode ReverseList(ListNode head) {
         if (head == null) return head;
 
         ListNode prev = null;
         ListNode curr = head;
-        ListNode next = null;
-        while (curr != null) {
-            next = curr.next;
-
-            if (next == null) {
-                curr.next = prev;
-                break;
-            }
-
+        ListNode next = curr.next;
+        while (next != null) {
             curr.next = prev;
 
             prev = curr;
             curr = next;
+            next = curr.next;
         }
+
+        curr.next = prev;
         return curr;
     }
 
--- src/CodingInterviews/_26_SubstructureInTree.java
+++ src/CodingInterviews/_26_SubstructureInTree.java
@@ -1,36 +1,24 @@
 package CodingInterviews;
 
-
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class _26_SubstructureInTree {
 
-    public boolean hasSubtree(TreeNode root1, TreeNode root2) {
-        if (root1 == null || root2 == null) return false;
-
-        Queue<TreeNode> q = new ArrayDeque();
-        q.offer(root1);
-        while (!q.isEmpty()) {
-            TreeNode curr = q.poll();
-
-            if (isSameTree(curr, root2)) return true;
-
-            if (curr.left != null) q.offer(curr.left);
-            if (curr.right != null) q.offer(curr.right);
-        }
-        return false;
+    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+        if (root2 == null) return false;
+        if (root1 == null) return false;
+        return HasSubtreeCore(root1, root2);
     }
 
-    private boolean isSameTree(TreeNode root1, TreeNode root2) {
+    public boolean HasSubtreeCore(TreeNode root1, TreeNode root2) {
         if (root2 == null) return true;
-
         if (root1 == null) return false;
 
-        if (root1.val == root2.val)
-            return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
-        else
-            return false;
+        if (root1.val == root2.val) {
+            boolean flag = HasSubtreeCore(root1.left, root2.left) &&
+                    HasSubtreeCore(root1.right, root2.right);
+            if (flag) return true;
+        }
+
+        return HasSubtreeCore(root1.left, root2) || HasSubtreeCore(root1.right, root2);
     }
 
 }
