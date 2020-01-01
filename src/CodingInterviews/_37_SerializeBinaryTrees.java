703550a476c101a0093f7a446807a1138168aafc
0x6c6868 <1023347025@qq.com>
2020-01-01 16:01:20 +0800

    code style google for 37

--- src/CodingInterviews/_37_SerializeBinaryTrees.java
+++ src/CodingInterviews/_37_SerializeBinaryTrees.java
@@ -2,38 +2,28 @@ package CodingInterviews;
 
 public class _37_SerializeBinaryTrees {
 
-    int index = 0;
+  private int index = -1;
 
-    String Serialize(TreeNode root) {
-        if (root == null) return "$";
-        return root.val + "," + Serialize(root.left) + "," + Serialize(root.right);
-    }
+  String Serialize(TreeNode root) {
+    if (root == null) return "$";
+    return root.val + "," + Serialize(root.left) + "," + Serialize(root.right);
+  }
 
-    TreeNode Deserialize(String str) {
-        if (str == null || str.length() == 0) return null;
-        String[] strList = str.split(",");
-        return deserializeCore(strList);
-    }
+  TreeNode Deserialize(String str) {
+    if (str == null) return null;
+    return DeserializeCore(str.split(","));
+  }
 
-    TreeNode deserializeCore(String[] strList) {
-        if (index >= strList.length) return null;
+  private TreeNode DeserializeCore(String[] array) {
+    index++;
+    if (index >= array.length) return null;
+    if (array[index].equals("$")) return null;
 
-        String val = strList[index];
-        if (val.equals("$")) return null;
+    TreeNode node = new TreeNode(Integer.valueOf(array[index]));
 
-        int valInt = Integer.parseInt(val);
-        TreeNode node = new TreeNode(valInt);
+    node.left = DeserializeCore(array);
+    node.right = DeserializeCore(array);
 
-        index += 1;
-        TreeNode leftNode = deserializeCore(strList);
-
-        index += 1;
-        TreeNode rightNode = deserializeCore(strList);
-
-        node.left = leftNode;
-        node.right = rightNode;
-
-        return node;
-    }
-
+    return node;
+  }
 }
