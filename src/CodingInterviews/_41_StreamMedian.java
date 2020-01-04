b6bae8f7062225ff3c67869f9764c5db0ba1a45a
0x6c6868 <1023347025@qq.com>
2020-01-04 20:27:37 +0800

    41 follow google style

--- src/CodingInterviews/_41_StreamMedian.java
+++ src/CodingInterviews/_41_StreamMedian.java
@@ -4,32 +4,28 @@ import java.util.PriorityQueue;
 
 public class _41_StreamMedian {
 
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((o1, o2) -> {
-        return o2.compareTo(o1);
-    });
-    PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+  private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+  private PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
 
-    public void Insert(Integer num) {
-        if (maxHeap.isEmpty()) {
-            maxHeap.offer(num);
-        } else {
-            if (maxHeap.peek() > num) {
-                maxHeap.offer(num);
-                if (maxHeap.size() - minHeap.size() > 1)
-                    minHeap.offer(maxHeap.poll());
-            } else {
-                minHeap.offer(num);
-                if (minHeap.size() - maxHeap.size() > 1)
-                    maxHeap.offer(minHeap.poll());
-            }
-        }
+  private boolean isEven = true;
+
+  public void Insert(Integer num) {
+    if (isEven) {
+      maxHeap.offer(num);
+
+      int max = maxHeap.poll();
+      minHeap.offer(max);
+    } else {
+      minHeap.offer(num);
+
+      int min = minHeap.poll();
+      maxHeap.offer(min);
     }
+    isEven = !isEven;
+  }
 
-    public Double GetMedian() {
-        if (minHeap.size() <= 0 && maxHeap.size() <= 0) return -1d;
-        if (maxHeap.size() - minHeap.size() == 1) return (double) maxHeap.peek();
-        if (minHeap.size() - maxHeap.size() == 1) return (double) minHeap.peek();
-        return ((double) minHeap.peek() + maxHeap.peek()) / 2;
-    }
-
+  public Double GetMedian() {
+    if (isEven) return (double) (minHeap.peek() + maxHeap.peek()) / 2;
+    return (double) minHeap.peek();
+  }
 }
