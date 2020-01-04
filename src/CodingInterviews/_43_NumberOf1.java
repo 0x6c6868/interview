1173985333cac070dbe01bf4f5cdf7dfde04a22e
0x6c6868 <1023347025@qq.com>
2020-01-04 21:15:13 +0800

    43 for google style

--- src/CodingInterviews/_43_NumberOf1.java
+++ src/CodingInterviews/_43_NumberOf1.java
@@ -2,18 +2,23 @@ package CodingInterviews;
 
 public class _43_NumberOf1 {
 
-    public int NumberOf1Between1AndN_Solution(int n) {
-        if (n == 0) return 0;
+  public int NumberOf1Between1AndN_Solution(int n) {
+    if (n == 0) return 0;
 
-        int rst = 0;
-        for (int i = 1; i <= n; i *= 10) {
-            int a = n / i;
-            int b = n % i;
+    int rst = 0;
+    for (int i = 1; i <= n; i *= 10) {
+      int high = n / i / 10;
+      int curr = n / i % 10;
+      int low = n % i;
 
-            rst += (a + 8) / 10 * i;
-            if (a % 10 == 1) rst += b + 1;
-        }
-        return rst;
+      if (curr == 0) {
+        rst += high * i;
+      } else if (curr == 1) {
+        rst += high * i + low + 1;
+      } else {
+        rst += (high + 1) * i;
+      }
     }
-
+    return rst;
+  }
 }
