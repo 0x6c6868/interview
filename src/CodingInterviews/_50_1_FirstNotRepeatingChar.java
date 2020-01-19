e59383e4b301122924c1a9517b287402ca76e796
0x6c6868 <1023347025@qq.com>
2020-01-19 23:49:31 +0800

    follow google style

--- src/CodingInterviews/_50_1_FirstNotRepeatingChar.java
+++ src/CodingInterviews/_50_1_FirstNotRepeatingChar.java
@@ -1,26 +1,11 @@
 package CodingInterviews;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class _50_1_FirstNotRepeatingChar {
 
-    public int FirstNotRepeatingChar(String str) {
-        if (str == null || str.length() == 0) return -1;
-
-        Map<Character, Integer> map = new HashMap();
-        for (int i = 0; i < str.length(); i++) {
-            Character currChar = str.charAt(i);
-            map.put(currChar, map.getOrDefault(currChar, 0) + 1);
-        }
-
-        for (int i = 0; i < str.length(); i++) {
-            Character currChar = str.charAt(i);
-            if (map.get(currChar) == 1)
-                return i;
-        }
-
-        return -1;
-    }
-
+  public int FirstNotRepeatingChar(String str) {
+    int[] counts = new int[58];
+    for (int i = 0; i < str.length(); i++) counts[str.charAt(i) - 'A'] += 1;
+    for (int i = 0; i < str.length(); i++) if (counts[str.charAt(i) - 'A'] == 1) return i;
+    return -1;
+  }
 }
