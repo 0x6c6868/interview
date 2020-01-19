ae07a3ec97028a525705adb97cfa51f0a30e5844
0x6c6868 <1023347025@qq.com>
2020-01-19 23:59:39 +0800

    follow google style

--- src/CodingInterviews/_50_2_FirstCharacterInStream.java
+++ src/CodingInterviews/_50_2_FirstCharacterInStream.java
@@ -1,26 +1,20 @@
 package CodingInterviews;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class _50_2_FirstCharacterInStream {
 
-    private Map<Character, Integer> charCount = new HashMap<>();
+  private List<Character> data = new ArrayList<>();
+  int[] counts = new int[128];
 
-    private List<Character> data = new ArrayList<>();
+  public void Insert(char ch) {
+    counts[ch]++;
+    data.add(ch);
+  }
 
-    public void Insert(char ch) {
-        charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
-        data.add(ch);
-    }
-
-    public char FirstAppearingOnce() {
-        for (Character ch : data) {
-            if (charCount.get(ch) == 1) return ch;
-        }
-        return '#';
-    }
-
+  public char FirstAppearingOnce() {
+    for (Character ch : data) if (counts[ch] == 1) return ch;
+    return '#';
+  }
 }
