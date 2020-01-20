9f1160b06858ab27b57a488de31e553363e77216
0x6c6868 <1023347025@qq.com>
2020-01-21 00:11:53 +0800

    google style & very slow speed

--- src/CodingInterviews/_51_InversePairs.java
+++ src/CodingInterviews/_51_InversePairs.java
@@ -2,39 +2,35 @@ package CodingInterviews;
 
 public class _51_InversePairs {
 
-    private int cnt;
+  public int InversePairs(int[] array) {
+    temp = new int[array.length];
+    Sort(array, 0, array.length - 1);
+    return cnt;
+  }
 
-    private void Sort(int[] array, int start, int end) {
-        if (start >= end) return;
-        int mid = (start + end) / 2;
-        Sort(array, start, mid);
-        Sort(array, mid + 1, end);
-        Merge(array, start, mid, end);
+  private int cnt;
+  private int[] temp;
+
+  private void Sort(int[] array, int start, int end) {
+    if (start >= end) return;
+    int mid = (start + end) / 2;
+    Sort(array, start, mid);
+    Sort(array, mid + 1, end);
+    Merge(array, start, mid, end);
+  }
+
+  private void Merge(int[] array, int start, int mid, int end) {
+    int k = end, i = mid, j = end;
+    while (i >= start && j >= mid + 1) {
+      if (array[i] > array[j]) {
+        temp[k--] = array[i--];
+        cnt = (cnt + j - mid) % 1000000007;
+      } else temp[k--] = array[j--];
     }
 
-    private void Merge(int[] array, int start, int mid, int end) {
-        int[] temp = new int[end - start + 1];
-        int k = 0, i = start, j = mid + 1;
-        while (i <= mid && j <= end) {
-            if (array[i] <= array[j])
-                temp[k++] = array[i++];
-            else {
-                temp[k++] = array[j++];
-                cnt = (cnt + (mid - i + 1)) % 1000000007;
-            }
-        }
-        while (i <= mid)
-            temp[k++] = array[i++];
-        while (j <= end)
-            temp[k++] = array[j++];
-        for (int l = 0; l < k; l++) {
-            array[start + l] = temp[l];
-        }
-    }
+    while (i >= start) temp[k--] = array[i--];
+    while (j >= mid + 1) temp[k--] = array[j--];
 
-    public int InversePairs(int[] array) {
-        Sort(array, 0, array.length - 1);
-        return cnt;
-    }
-
+    for (int l = start; l <= end; l++) array[l] = temp[l];
+  }
 }
