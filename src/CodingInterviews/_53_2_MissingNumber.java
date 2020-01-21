6c61c733cef243415bc2e9c7e2e538fb4327fd4c
0x6c6868 <1023347025@qq.com>
2020-01-21 23:40:16 +0800

    follow google

--- src/CodingInterviews/_53_2_MissingNumber.java
+++ src/CodingInterviews/_53_2_MissingNumber.java
@@ -2,47 +2,40 @@ package CodingInterviews;
 
 public class _53_2_MissingNumber {
 
-    public int GetMissingNumber(int[] array) {
-        if (array == null || array.length == 0) return -1;
+  public int GetMissingNumber(int[] array) {
+    if (array == null || array.length == 0) return -1;
 
-        int start = 0;
-        int end = array.length - 1;
-        int mid = start;
-        while (start <= end) {
-            mid = (end - start) / 2 + start;
-            if (array[mid] == mid) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-
-        if (array[mid] == mid) return mid + 1;
-
-        return mid;
+    int start = 0, end = array.length - 1, mid = start;
+    while (start <= end) {
+      mid = (end - start) / 2 + start;
+      if (array[mid] == mid) start = mid + 1;
+      else end = mid - 1;
     }
 
-    public static void main(String[] args) {
-        _53_2_MissingNumber p = new _53_2_MissingNumber();
-        int[] array = null;
+    if (array[mid] == mid) return mid + 1;
+    return mid;
+  }
 
-        array = new int[]{1, 2, 3, 4, 5};
-        System.out.println(p.GetMissingNumber(array) == 0 ? "Passed" : "Failed");
+  public static void main(String[] args) {
+    _53_2_MissingNumber p = new _53_2_MissingNumber();
+    int[] array;
 
-        array = new int[]{0, 1, 2, 3, 4};
-        System.out.println(p.GetMissingNumber(array) == 5 ? "Passed" : "Failed");
+    array = new int[] {1, 2, 3, 4, 5};
+    System.out.println(p.GetMissingNumber(array) == 0 ? "Passed" : "Failed");
 
-        array = new int[]{0, 1, 2, 4, 5};
-        System.out.println(p.GetMissingNumber(array) == 3 ? "Passed" : "Failed");
+    array = new int[] {0, 1, 2, 3, 4};
+    System.out.println(p.GetMissingNumber(array) == 5 ? "Passed" : "Failed");
 
-        array = new int[]{1};
-        System.out.println(p.GetMissingNumber(array) == 0 ? "Passed" : "Failed");
+    array = new int[] {0, 1, 2, 4, 5};
+    System.out.println(p.GetMissingNumber(array) == 3 ? "Passed" : "Failed");
 
-        array = new int[]{0};
-        System.out.println(p.GetMissingNumber(array) == 1 ? "Passed" : "Failed");
+    array = new int[] {1};
+    System.out.println(p.GetMissingNumber(array) == 0 ? "Passed" : "Failed");
 
-        array = null;
-        System.out.println(p.GetMissingNumber(array) == -1 ? "Passed" : "Failed");
-    }
+    array = new int[] {0};
+    System.out.println(p.GetMissingNumber(array) == 1 ? "Passed" : "Failed");
 
+    array = null;
+    System.out.println(p.GetMissingNumber(array) == -1 ? "Passed" : "Failed");
+  }
 }
--- src/CodingInterviews/_53_3_IntegerIdenticalToIndex.java
+++ src/CodingInterviews/_53_3_IntegerIdenticalToIndex.java
@@ -2,47 +2,44 @@ package CodingInterviews;
 
 public class _53_3_IntegerIdenticalToIndex {
 
-    public int GetNumberSameAsIndex(int[] array) {
-        if (array == null || array.length == 0) return -1;
+  public int GetNumberSameAsIndex(int[] array) {
+    if (array == null || array.length == 0) return -1;
 
-        int start = 0;
-        int end = array.length - 1;
-        while (start <= end) {
-            int mid = (end - start) / 2 + start;
-
-            if (array[mid] == mid) return mid;
-
-            if (array[mid] > mid) end = mid - 1;
-            else start = mid + 1;
-        }
-
-        return -1;
+    int start = 0;
+    int end = array.length - 1;
+    while (start <= end) {
+      int mid = (end - start) / 2 + start;
+      if (array[mid] == mid) return mid;
+      if (array[mid] > mid) end = mid - 1;
+      else start = mid + 1;
     }
 
-    public static void main(String[] args) {
-        _53_3_IntegerIdenticalToIndex p = new _53_3_IntegerIdenticalToIndex();
-        int[] array = null;
+    return -1;
+  }
 
-        array = new int[]{-3, -1, 1, 3, 5};
-        System.out.println(p.GetNumberSameAsIndex(array) == 3 ? "Passed" : "Failed");
+  public static void main(String[] args) {
+    _53_3_IntegerIdenticalToIndex p = new _53_3_IntegerIdenticalToIndex();
+    int[] array;
 
-        array = new int[]{0, 1, 3, 5, 6};
-        System.out.println(p.GetNumberSameAsIndex(array) == 0 ? "Passed" : "Failed");
+    array = new int[] {-3, -1, 1, 3, 5};
+    System.out.println(p.GetNumberSameAsIndex(array) == 3 ? "Passed" : "Failed");
 
-        array = new int[]{-1, 0, 1, 2, 4};
-        System.out.println(p.GetNumberSameAsIndex(array) == 4 ? "Passed" : "Failed");
+    array = new int[] {0, 1, 3, 5, 6};
+    System.out.println(p.GetNumberSameAsIndex(array) == 0 ? "Passed" : "Failed");
 
-        array = new int[]{-1, 0, 1, 2, 5};
-        System.out.println(p.GetNumberSameAsIndex(array) == -1 ? "Passed" : "Failed");
+    array = new int[] {-1, 0, 1, 2, 4};
+    System.out.println(p.GetNumberSameAsIndex(array) == 4 ? "Passed" : "Failed");
 
-        array = new int[]{0};
-        System.out.println(p.GetNumberSameAsIndex(array) == 0 ? "Passed" : "Failed");
+    array = new int[] {-1, 0, 1, 2, 5};
+    System.out.println(p.GetNumberSameAsIndex(array) == -1 ? "Passed" : "Failed");
 
-        array = new int[]{10};
-        System.out.println(p.GetNumberSameAsIndex(array) == -1 ? "Passed" : "Failed");
+    array = new int[] {0};
+    System.out.println(p.GetNumberSameAsIndex(array) == 0 ? "Passed" : "Failed");
 
-        array = null;
-        System.out.println(p.GetNumberSameAsIndex(array) == -1 ? "Passed" : "Failed");
-    }
+    array = new int[] {10};
+    System.out.println(p.GetNumberSameAsIndex(array) == -1 ? "Passed" : "Failed");
 
+    array = null;
+    System.out.println(p.GetNumberSameAsIndex(array) == -1 ? "Passed" : "Failed");
+  }
 }
