be9b3f6fe94d4b8d9bf3a1a67ff3288879d8e14e
0x6c6868 <1023347025@qq.com>
2020-01-22 00:00:01 +0800

    follow google & less code

--- src/CodingInterviews/_55_1_TreeDepth.java
+++ src/CodingInterviews/_55_1_TreeDepth.java
@@ -2,13 +2,8 @@ package CodingInterviews;
 
 public class _55_1_TreeDepth {
 
-    public int TreeDepth(TreeNode root) {
-        if (root == null) return 0;
-
-        int leftDepth = TreeDepth(root.left);
-        int rightDepth = TreeDepth(root.right);
-
-        return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
-    }
-
+  public int TreeDepth(TreeNode root) {
+    if (root == null) return 0;
+    return Math.max(TreeDepth(root.left) + 1, TreeDepth(root.right) + 1);
+  }
 }
