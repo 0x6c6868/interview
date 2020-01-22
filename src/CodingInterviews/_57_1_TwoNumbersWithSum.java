0a752fed10c53c6f3158cb04c0f6a9c1f9539c8d
0x6c6868 <1023347025@qq.com>
2020-01-23 00:06:33 +0800

    google style

--- src/CodingInterviews/_57_1_TwoNumbersWithSum.java
+++ src/CodingInterviews/_57_1_TwoNumbersWithSum.java
@@ -4,28 +4,24 @@ import java.util.ArrayList;
 
 public class _57_1_TwoNumbersWithSum {
 
-    public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
-        ArrayList<Integer> rst = new ArrayList<>();
+  public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
+    ArrayList<Integer> rst = new ArrayList<>();
 
-        if (array == null || array.length == 0) return rst;
+    if (array == null || array.length == 0) return rst;
 
-        int start = 0;
-        int end = array.length - 1;
-        while (start < end) {
-            if (array[start] + array[end] == sum) {
-                rst.add(array[start]);
-                rst.add(array[end]);
-                break;
-            }
+    int start = 0;
+    int end = array.length - 1;
+    while (start < end) {
+      if (array[start] + array[end] == sum) {
+        rst.add(array[start]);
+        rst.add(array[end]);
+        break;
+      }
 
-            if (array[start] + array[end] > sum) {
-                end--;
-            } else {
-                start++;
-            }
-        }
-
-        return rst;
+      if (array[start] + array[end] > sum) end--;
+      else start++;
     }
 
+    return rst;
+  }
 }
