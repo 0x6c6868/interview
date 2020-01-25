60577077390ccbcd19ea8fc2ae3b49fe8f62623f
0x6c6868 <1023347025@qq.com>
2020-01-26 00:07:36 +0800

    google style

--- src/CodingInterviews/_59_2_QueueWithMax.java
+++ src/CodingInterviews/_59_2_QueueWithMax.java
@@ -6,89 +6,84 @@ import java.util.Queue;
 
 public class _59_2_QueueWithMax {
 
-    private Deque<Integer> maxDeque = new LinkedList();
-    private Queue<Integer> dataQueue = new LinkedList();
+  private Deque<Integer> maxDeque = new LinkedList();
+  private Queue<Integer> dataQueue = new LinkedList();
 
-    public int pop_front() {
+  public int pop_front() {
+    int val = dataQueue.poll();
+    if (val == maxDeque.getFirst()) maxDeque.pop();
 
-        int val = dataQueue.poll();
-        if (val == maxDeque.getFirst()) {
-            maxDeque.pop();
-        }
+    return val;
+  }
 
-        return val;
-    }
+  public void push_back(int val) {
+    while (!maxDeque.isEmpty() && val > maxDeque.peekLast()) maxDeque.pollLast();
 
-    public void push_back(int val) {
-        while (!maxDeque.isEmpty() && val > maxDeque.peekLast()) {
-            maxDeque.pollLast();
-        }
-        maxDeque.offer(val);
+    maxDeque.offer(val);
+    dataQueue.offer(val);
+  }
 
-        dataQueue.offer(val);
-    }
+  public int max() {
+    return maxDeque.getFirst();
+  }
 
-    public int max() {
-        return maxDeque.getFirst();
-    }
+  public static void main(String[] args) {
+    _59_2_QueueWithMax q = new _59_2_QueueWithMax();
 
-    public static void main(String[] args) {
-        _59_2_QueueWithMax q = new _59_2_QueueWithMax();
+    // {2}
+    q.push_back(2);
+    System.out.println(q.max() == 2);
 
-        // {2}
-        q.push_back(2);
-        System.out.println(q.max() == 2);
+    // {2, 3}
+    q.push_back(3);
+    System.out.println(q.max() == 3);
 
-        // {2, 3}
-        q.push_back(3);
-        System.out.println(q.max() == 3);
+    // {2, 3, 4}
+    q.push_back(4);
+    System.out.println(q.max() == 4);
 
-        // {2, 3, 4}
-        q.push_back(4);
-        System.out.println(q.max() == 4);
+    // {2, 3, 4, 2}
+    q.push_back(2);
+    System.out.println(q.max() == 4);
 
-        // {2, 3, 4, 2}
-        q.push_back(2);
-        System.out.println(q.max() == 4);
+    // {3, 4, 2}
+    q.pop_front();
+    System.out.println(q.max() == 4);
 
-        // {3, 4, 2}
-        q.pop_front();
-        System.out.println(q.max() == 4);
+    // {4, 2}
+    q.pop_front();
+    System.out.println(q.max() == 4);
 
-        // {4, 2}
-        q.pop_front();
-        System.out.println(q.max() == 4);
+    // {2}
+    q.pop_front();
+    System.out.println(q.max() == 2);
 
-        // {2}
-        q.pop_front();
-        System.out.println(q.max() == 2);
+    // {2, 6}
+    q.push_back(6);
+    System.out.println(q.max() == 6);
 
-        // {2, 6}
-        q.push_back(6);
-        System.out.println(q.max() == 6);
+    // {2, 6, 2}
+    q.push_back(2);
+    System.out.println(q.max() == 6);
 
-        // {2, 6, 2}
-        q.push_back(2);
-        System.out.println(q.max() == 6);
+    // {2, 6, 2, 5}
+    q.push_back(5);
+    System.out.println(q.max() == 6);
 
-        // {2, 6, 2, 5}
-        q.push_back(5);
-        System.out.println(q.max() == 6);
+    // {6, 2, 5}
+    q.pop_front();
+    System.out.println(q.max() == 6);
 
-        // {6, 2, 5}
-        q.pop_front();
-        System.out.println(q.max() == 6);
+    // {2, 5}
+    q.pop_front();
+    System.out.println(q.max() == 5);
 
-        // {2, 5}
-        q.pop_front();
-        System.out.println(q.max() == 5);
+    // {5}
+    q.pop_front();
+    System.out.println(q.max() == 5);
 
-        // {5}
-        q.pop_front();
-        System.out.println(q.max() == 5);
-
-        // {5, 1}
-        q.push_back(1);
-        System.out.println(q.max() == 5);
-    }
+    // {5, 1}
+    q.push_back(1);
+    System.out.println(q.max() == 5);
+  }
 }
--- src/CodingInterviews/_60_DicesProbability.java
+++ src/CodingInterviews/_60_DicesProbability.java
@@ -3,49 +3,41 @@ package CodingInterviews;
 import java.util.HashMap;
 import java.util.Map;
 
+// https://www.cnblogs.com/wangkundentisy/p/9378886.html
 public class _60_DicesProbability {
 
-    public Map<Integer, Double> PrintProbability(int number) {
-        Map rst = new HashMap();
+  public Map<Integer, Double> PrintProbability(int number) {
+    Map<Integer, Double> rst = new HashMap();
+    if (number <= 0) return rst;
 
-        if (number <= 0) return rst;
+    // dp init
+    int[][] arr = new int[number + 1][number * 6 + 1];
+    for (int i = 1; i <= 6; i++) arr[1][i] = 1;
 
-        // https://www.cnblogs.com/wangkundentisy/p/9378886.html
-        int[][] arr = new int[number + 1][number * 6 + 1];
-        for (int i = 1; i <= 6; i++) {
-            arr[1][i] = 1;
-        }
+    // dp calc
+    for (int i = 2; i <= number; i++)
+      for (int j = i; j <= 6 * i; j++)
+        for (int k = 1; k <= 6 && j - k >= 1; k++) arr[i][j] += arr[i - 1][j - k];
 
-        for (int i = 2; i <= number; i++) {
-            for (int j = i; j <= 6 * i; j++) {
-                for (int k = 1; k <= 6 && j - k >= 1; k++) {
-                    arr[i][j] += arr[i - 1][j - k];
-                }
-            }
-        }
+    // sum
+    int sum = 0;
+    for (int i = number; i <= 6 * number; i++) sum += arr[number][i];
 
-        int sum = 0;
-        for (int i = number; i <= 6 * number; i++) {
-            sum += arr[number][i];
-        }
+    // build rst
+    for (int i = number; i <= 6 * number; i++) rst.put(i, (double) arr[number][i] / sum);
+    return rst;
+  }
 
-        for (int i = number; i <= 6 * number; i++) {
-            rst.put(i, (double) arr[number][i] / sum);
-        }
+  public static void main(String[] args) {
 
-        return rst;
-    }
+    _60_DicesProbability p = new _60_DicesProbability();
 
-    public static void main(String[] args) {
+    p.PrintProbability(1);
+    p.PrintProbability(2);
+    p.PrintProbability(3);
+    p.PrintProbability(4);
 
-        _60_DicesProbability p = new _60_DicesProbability();
-
-        p.PrintProbability(1);
-        p.PrintProbability(2);
-        p.PrintProbability(3);
-        p.PrintProbability(4);
-
-        p.PrintProbability(11);
-        p.PrintProbability(0);
-    }
+    p.PrintProbability(11);
+    p.PrintProbability(0);
+  }
 }
