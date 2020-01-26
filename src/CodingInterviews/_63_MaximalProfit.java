8a46355c0fdeb475601a5c9fa4932d675a5979dd
0x6c6868 <1023347025@qq.com>
2020-01-26 15:23:14 +0800

    google style & leetcode & less code

--- src/CodingInterviews/_63_MaximalProfit.java
+++ src/CodingInterviews/_63_MaximalProfit.java
@@ -1,47 +1,15 @@
 package CodingInterviews;
 
+// leetcode 121
 public class _63_MaximalProfit {
 
-    public int MaxDiff(int[] numbers) {
-        if (numbers == null || numbers.length < 2) return 0;
+  public int maxProfit(int[] prices) {
+    int maxProfit = 0, minPrice = Integer.MAX_VALUE;
 
-        int min = numbers[0];
-        int maxDiff = Integer.MIN_VALUE;
-        for (int i = 1; i < numbers.length; i++) {
-            if (min > numbers[i - 1]) min = numbers[i - 1];
-
-            if (numbers[i] - min > maxDiff) maxDiff = numbers[i] - min;
-        }
-        return maxDiff;
+    for (int i = 0; i < prices.length; i++) {
+      minPrice = Math.min(minPrice, prices[i]);
+      maxProfit = Math.max(maxProfit, prices[i] - minPrice);
     }
-
-    public static void main(String[] args) {
-        _63_MaximalProfit p = new _63_MaximalProfit();
-        int[] numbers = null;
-
-        numbers = new int[]{4, 1, 3, 2, 5};
-        System.out.println(p.MaxDiff(numbers) == 4);
-
-        numbers = new int[]{1, 2, 4, 7, 11, 16};
-        System.out.println(p.MaxDiff(numbers) == 15);
-
-        numbers = new int[]{16, 11, 7, 4, 2, 1};
-        System.out.println(p.MaxDiff(numbers) == -1);
-
-        numbers = new int[]{16, 16, 16, 16, 16};
-        System.out.println(p.MaxDiff(numbers) == 0);
-
-        numbers = new int[]{9, 11, 5, 7, 16, 1, 4, 2};
-        System.out.println(p.MaxDiff(numbers) == 11);
-
-        numbers = new int[]{2, 4};
-        System.out.println(p.MaxDiff(numbers) == 2);
-
-        numbers = new int[]{4, 2};
-        System.out.println(p.MaxDiff(numbers) == -2);
-
-        numbers = null;
-        System.out.println(p.MaxDiff(numbers) == 0);
-    }
-
+    return maxProfit;
+  }
 }
