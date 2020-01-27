d2b60958c771fb4c299200c4623f3c6437502fbc
0x6c6868 <1023347025@qq.com>
2020-01-27 20:51:42 +0800

    clean code

--- src/CodingInterviews/_18_2_DeleteDuplicatedNode.java
+++ src/CodingInterviews/_18_2_DeleteDuplicatedNode.java
@@ -2,7 +2,6 @@ package CodingInterviews;
 
 // 删除链表中重复的结点
 public class _18_2_DeleteDuplicatedNode {
-
   public ListNode deleteDuplication(ListNode pHead) {
     ListNode dummyHead = new ListNode(0);
     dummyHead.next = pHead;
@@ -11,14 +10,15 @@ public class _18_2_DeleteDuplicatedNode {
     ListNode last = pHead;
 
     while (last != null) {
-      if (last.next != null && last.val == last.next.val) {
-        while (last.next != null && last.val == last.next.val) last = last.next;
-        prev.next = last.next;
-        last = last.next;
-      } else {
+      if (last.next == null || last.val != last.next.val) {
         prev = prev.next;
         last = last.next;
+        continue;
       }
+
+      while (last.next != null && last.val == last.next.val) last = last.next;
+      prev.next = last.next;
+      last = last.next;
     }
     return dummyHead.next;
   }
