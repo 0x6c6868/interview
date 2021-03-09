16bed86cb2e064b0391cffa4e5e328365a833ba9
0x6c6868 <1023347025@qq.com>
2021-03-09 21:10:55 +0800

    count one

---
+++ src/BeautyOfProgramming/_2_1_CountOne.java
@@ -0,0 +1,20 @@
+package BeautyOfProgramming;
+
+/**
+ * 求二进制数中1的个数
+ */
+public class _2_1_CountOne {
+    public static void main(String[] args) {
+        int num = 0b10100010;
+        System.out.print(String.format("count:%s", process(num)));
+    }
+
+    private static int process(int num) {
+        int rst = 0;
+        while (num != 0) {
+            num &= (num - 1);
+            rst++;
+        }
+        return rst;
+    }
+}
